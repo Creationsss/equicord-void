8351aeb17ec7a1b5722ea7bd53ae824a86cd3da6
creations <creations@creations.works>
2026-02-23 15:19:25 -0500

    [losslesscut]

---
+++ srcpkgs/losslesscut/files/losslesscut.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ulimit -c 0
+
+PARAMS="--ozone-platform=x11"
+if [ "$USE_X11" = "false" ]; then
+	PARAMS="--enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=auto"
+fi
+
+exec /usr/lib/losslesscut/losslesscut $PARAMS "$@"
